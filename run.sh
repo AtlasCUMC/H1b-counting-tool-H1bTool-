a4c3a1697a5e5af81df80050d2a258f2c95d8612
Atlas Khan <atlas.akhan@gmail.com>
2018-10-27 17:54:08 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+
+# CERTIFIED
+#python ./src/H1bTools.v3.py -I /input/h1b_input.csv -out1 /output/top_10_occupations.txt -out2 /output/top_10_states.txt
 
